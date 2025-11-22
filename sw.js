7f90cd317b82f5f480e2ca03d8e9f6c96416e0dd
pipytako <mimihapaonline@gmail.com>
2025-11-22 11:10:04 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+
+const CACHE_NAME = 'PipTako🍑';
+const urlsToCache = [
+  '/',
+  'piptako.png'
+];
+
+// Install Event: Cache the core assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Event: Serve from cache, fall back to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return the response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
